1f891464d801700659755259a9c9c90570f80031
PedroTech <tiopedrotech@gmail.com>
2026-07-31 18:01:41 -0300

    Update ex05.c

--- condicionais/ex05.c
+++ condicionais/ex05.c
@@ -21,28 +21,4 @@ int main(){
         printf("Menor valor = %d ", num3);
     }
     return 0;
-=======
-#include <stdio.h>
-
-int main(){
-    int num1, num2, num3;
-
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &num1);
-    printf("Digite o segundo numero: ");
-    scanf("%d", &num2);
-    printf("Digite o terceiro numero: ");
-    scanf("%d", &num3);
-
-    if (num1 <= num2 && num1 <= num3){
-        printf("Menor valor = %d ", num1);
-    } 
-    else if (num2 <= num1 && num2 <= num3){
-        printf("Menor valor = %d ", num2);
-    }
-    else {
-        printf("Menor valor = %d ", num3);
-    }
-    return 0;
->>>>>>> 978699a15070938943eace52fa7d42026c3dadac
 }
